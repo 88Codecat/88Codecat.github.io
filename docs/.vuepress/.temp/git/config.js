5682f682135b5e2f85006627baa0dd8d952d5281
88Codecat <1925644842@qq.com>
2025-05-01 12:49:02 +0800

    删除a.md

--- docs/.vuepress/.temp/git/config.js
+++ docs/.vuepress/.temp/git/config.js
@@ -1,9 +1,7 @@
 import { GitContributors } from "D:/MyBlog/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_3af858c91994abeca046bd8439278c94/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "D:/MyBlog/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_3af858c91994abeca046bd8439278c94/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
 
 export default {
   enhance: ({ app }) => {
     app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
   },
 };
